09dd6806ff0acc3f80436b1f4cded22ee728b17a
Muhammad Wasif <mwasif66625426@gmail.com>
2025-10-21 11:44:02 +0500

    changes fixed

--- src/data/MenuData.ts
+++ src/data/MenuData.ts
@@ -1,83 +1,118 @@
-
 interface MenuItem {
-    id: number;
-    title: string;
+  id: number;
+  title: string;
+  link: string;
+  has_dropdown: boolean;
+  sub_menus?: {
     link: string;
-    has_dropdown: boolean;
-    sub_menus?: {
-        link: string;
-        title: string;
-    }[];
+    title: string;
+  }[];
 }
 
 const menu_data: MenuItem[] = [
-    {
-        id: 1,
-        title: "Home",
-        link: "#",
-        has_dropdown: true,
-        sub_menus: [
-            { link: "/", title: "Home One" },
-            { link: "/home-two", title: "Home Two" },
-            { link: "/home-three", title: "Home Three" },
-            { link: "/home-four", title: "Home Four" },
-            { link: "/home-five", title: "Home Five" },
-            { link: "/home-six", title: "Home Six" },
-            { link: "/home-seven", title: "Home Seven" },
-        ],
-    },
-    {
-        id: 2,
-        title: "Features",
-        link: "#",
-        has_dropdown: true,
-        sub_menus: [
-            { link: "/hotel-grid", title: "Hotel Grid" },
-            { link: "/tour-grid-1", title: "Tour Grid One" },
-            { link: "/tour-grid-2", title: "Tour Grid Two" },
-            { link: "/map-listing", title: "Hotel Listing" },
-            { link: "/tour-details", title: "Tour Details One" },
-            { link: "/tour-details-2", title: "Tour Details Two" },
-        ],
-    },
-    {
-        id: 3,
-        title: "Pages",
-        link: "#",
-        has_dropdown: true,
-        sub_menus: [
-            { link: "/about", title: "About" },
-            { link: "/team", title: "Team" },
-            { link: "/team-details", title: "Team Details" },
-            { link: "/shop", title: "Shop" },
-            { link: "/shop-details", title: "Shop Details" },
-            { link: "/cart", title: "Cart" },
-            { link: "/wishlist", title: "Wishlist" },
-            { link: "/checkout", title: "Checkout" },
-            { link: "/pricing", title: "Pricing" },
-            { link: "/faq", title: "Faq" },
-            { link: "/login", title: "Log In" },
-            { link: "/register", title: "Register" },
-            { link: "/no-found", title: "Error" },
-        ],
-    },
-    {
-        id: 4,
-        title: "Blogs",
-        link: "#",
-        has_dropdown: true,
-        sub_menus: [
-            { link: "/blog-grid", title: "Blog Grid" },
-            { link: "/blog-standard", title: "Blog Standard" },
-            { link: "/blog-details", title: "Blog Details" },
-        ],
-    },
-    {
-        id: 5,
-        has_dropdown: false,
-        title: "Contact",
-        link: "/contact",
-    },
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+    has_dropdown: false,
+    // sub_menus: [
+    //     { link: "/", title: "Home One" },
+    //     { link: "/home-two", title: "Home Two" },
+    //     { link: "/home-three", title: "Home Three" },
+    //     { link: "/home-four", title: "Home Four" },
+    //     { link: "/home-five", title: "Home Five" },
+    //     { link: "/home-six", title: "Home Six" },
+    //     { link: "/home-seven", title: "Home Seven" },
+    // ],
+  },
+  {
+    id: 2,
+    title: "About",
+    link: "#",
+    has_dropdown: false,
+    // sub_menus: [
+    //   { link: "/hotel-grid", title: "Hotel Grid" },
+    //   { link: "/tour-grid-1", title: "Tour Grid One" },
+    //   { link: "/tour-grid-2", title: "Tour Grid Two" },
+    //   { link: "/map-listing", title: "Hotel Listing" },
+    //   { link: "/tour-details", title: "Tour Details One" },
+    //   { link: "/tour-details-2", title: "Tour Details Two" },
+    // ],
+  },
+  {
+    id: 3,
+    title: "Listing",
+    link: "#",
+    has_dropdown: false,
+  },
+  {
+    id: 4,
+    title: "Investment Opportunities",
+    link: "#",
+    has_dropdown: false,
+  },
+  {
+    id: 5,
+    title: "Premium Packages",
+    link: "#",
+    has_dropdown: false,
+  },
+  {
+    id: 6,
+    title: "Pricing",
+    link: "#",
+    has_dropdown: false,
+  },
+  {
+    id: 7,
+    title: "Media",
+    link: "#",
+    has_dropdown: false,
+  },
+  //   {
+  //     id: 8,
+  //     title: "Investment Opportunities",
+  //     link: "#",
+  //     has_dropdown: false,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Pages",
+  //     link: "#",
+  //     has_dropdown: true,
+  //     sub_menus: [
+  //       { link: "/about", title: "About" },
+  //       { link: "/team", title: "Team" },
+  //       { link: "/team-details", title: "Team Details" },
+  //       { link: "/shop", title: "Shop" },
+  //       { link: "/shop-details", title: "Shop Details" },
+  //       { link: "/cart", title: "Cart" },
+  //       { link: "/wishlist", title: "Wishlist" },
+  //       { link: "/checkout", title: "Checkout" },
+  //       { link: "/pricing", title: "Pricing" },
+  //       { link: "/faq", title: "Faq" },
+  //       { link: "/login", title: "Log In" },
+  //       { link: "/register", title: "Register" },
+  //       { link: "/no-found", title: "Error" },
+  //     ],
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Blogs",
+  //     link: "#",
+  //     has_dropdown: true,
+  //     sub_menus: [
+  //       { link: "/blog-grid", title: "Blog Grid" },
+  //       { link: "/blog-standard", title: "Blog Standard" },
+  //       { link: "/blog-details", title: "Blog Details" },
+  //     ],
+  //   },
+  {
+    id: 8,
+    has_dropdown: false,
+    title: "Contact",
+    link: "/contact",
+  },
 ];
 
 export default menu_data;
