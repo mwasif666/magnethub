5a7cda159b1e5c75a4eeb3c93b96b6cf4cae9b6b
Muhammad Wasif <mwasif66625426@gmail.com>
2025-10-29 00:32:02 +0500

    update

--- src/components/homes/home-one/About.tsx
+++ src/components/homes/home-one/About.tsx
@@ -1,49 +1,12 @@
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import Link from "next/link";
-
-import shape_1 from "@/assets/img/about/su/right-shape.png";
-import shape_2 from "@/assets/img/about/su/shape.png";
-import shape_3 from "@/assets/img/about/su/shape-2.png";
-import about_thumb from "@/assets/img/about/premium.gif";
-import icon_1 from "@/assets/img/about/su/fun-3.png";
-import icon_2 from "@/assets/img/about/su/fun-2.png";
-import icon_3 from "@/assets/img/about/su/fun-1.png";
-import author from "@/assets/img/about/su/author.jpg";
-
-interface DataType {
-  id: number;
-  icon: StaticImageData;
-  title: string;
-  count: number;
-}
-
-const feature_data: DataType[] = [
-  {
-    id: 1,
-    icon: icon_1,
-    title: "Top Destination",
-    count: 5000,
-  },
-  {
-    id: 2,
-    icon: icon_2,
-    title: "Booking Completed",
-    count: 3000,
-  },
-  {
-    id: 3,
-    icon: icon_3,
-    title: "Top Destination",
-    count: 5000,
-  },
-];
 
 const About = () => {
   return (
     <div className="tg-about-area p-relative z-index-1 pb-80">
       <Image
         className="tg-about-su-right-shape d-none d-xl-block"
-        src={shape_1}
+        src="https://thumbs.dreamstime.com/b/vector-illustration-avatar-dummy-logo-set-image-stock-isolated-object-icon-collection-137161298.jpg"
         alt=""
       />
       <div className="container pt-80">
@@ -54,7 +17,10 @@ const About = () => {
               data-wow-delay=".4s"
               data-wow-duration=".6s"
             >
-              <Image src={about_thumb} alt="" />
+              <Image
+                src="https://thumbs.dreamstime.com/b/vector-illustration-avatar-dummy-logo-set-image-stock-isolated-object-icon-collection-137161298.jpg"
+                alt=""
+              />
             </div>
           </div>
           <div className="col-lg-7">
@@ -70,8 +36,8 @@ const About = () => {
                 </h2>
                 <p className="tg-section-su-para mb-10">
                   Magnate Hub is the ultimate platform for entrepreneurs and
-                  business owners. Whether you're looking to buy or sell a
-                  business, find investors, or raise capital, we've got you
+                  business owners. Whether you&apos;re looking to buy or sell a
+                  business, find investors, or raise capital, we&apos;ve got you
                   covered.
                   <br />
                   With a user-friendly interface and a vast network of potential
