67ddf801d49be5325e319cea5e47ef5b3fa2ab31
SyedAsadNasirJafri <syedjafri5003@gmail.com>
2025-11-03 11:10:15 +0500

    changes

---
+++ src/components/pages/media/index.tsx
@@ -0,0 +1,20 @@
+import BreadCrumb from "@/components/common/BreadCrumb"
+import HeaderThree from "@/layouts/headers/HeaderThree"
+import BlogDetailsArea from "./BlogDetailsArea"
+import FooterFive from "@/layouts/footers/FooterFive"
+import HeaderOne from "@/layouts/headers/HeaderOne"
+
+const Media = () => {
+  return (
+    <>
+      <HeaderOne />
+      <main>
+        <BreadCrumb title="Media" sub_title="Exploring The Green Spac Realar Residence Area Harmony" />
+        <BlogDetailsArea />
+      </main>
+      <FooterFive />
+    </>
+  )
+}
+
+export default Media
