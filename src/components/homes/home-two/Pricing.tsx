5d3c82ddff19825f82a38919b13cc34553d0a868
Muhammad Wasif <mwasif66625426@gmail.com>
2025-11-14 21:41:47 +0500

    prcining done

--- src/components/homes/home-two/Pricing.tsx
+++ src/components/homes/home-two/Pricing.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import pricing_data from "@/data/PricingData";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 
 type PricingItem = {
   id: number;
@@ -81,7 +83,6 @@ const PricingCard: React.FC<PricingCardProps> = ({ item }) => {
         <div className="tg-pricing-head">
           <h4 className="tg-pricing-title mb-15">{item.title}</h4>
         </div>
-
         <div className="tg-pricing-price mb-25">
           <h2>
             <span>$</span>
@@ -89,7 +90,6 @@ const PricingCard: React.FC<PricingCardProps> = ({ item }) => {
           </h2>
           <span className="dates">/month *</span>
         </div>
-
         <div className="tg-pricing-btns mb-40">
           <span
             className="tg-btn text-center w-100"
@@ -112,55 +112,49 @@ const PricingCard: React.FC<PricingCardProps> = ({ item }) => {
               </button>
             )}
           </p>
+          <div className="d-flex align-items-center gap-2">
+            <p>
+              {" "}
+              <strong
+                style={{
+                  color: "#560ce3",
+                }}
+              >
+                Includes:
+              </strong>{" "}
+            </p>
+            <p>10% GST</p>
+          </div>
 
-          <style>{`
-    .customScroll {
-      height: 400px;
-      overflow-y: scroll;
-    }
+          <SimpleBar autoHide={true}>
+            <ul className="customScroll">
+              {item.list.map((list, i) => (
+                <li key={i}>
+                  <span className="icon">
+                    {" "}
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      {" "}
+                      <path
+                        d="M17 8.26858V9.00458C16.999 10.7297 16.4404 12.4083 15.4075 13.79C14.3745 15.1718 12.9226 16.1826 11.2683 16.6717C9.61394 17.1608 7.8458 17.1021 6.22757 16.5042C4.60934 15.9064 3.22772 14.8015 2.28877 13.3542C1.34981 11.907 0.903833 10.195 1.01734 8.47363C1.13085 6.75223 1.79777 5.11364 2.91862 3.80224C4.03948 2.49083 5.55423 1.57688 7.23695 1.1967C8.91967 0.816507 10.6802 0.990449 12.256 1.69258M17 2.60458L9 10.6126L6.6 8.21258"
+                        stroke="#560CE3"
+                        strokeWidth="1.8"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />{" "}
+                    </svg>{" "}
+                  </span>
+                  <span>{list}</span>
+                </li>
+              ))}
+            </ul>
+          </SimpleBar>
 
-    .customScroll::-webkit-scrollbar {
-      width: 8px;
-    }
-
-    .customScroll::-webkit-scrollbar-track {
-      background: #f0f0f0;
-      border-radius: 4px;
-    }
-
-    .customScroll::-webkit-scrollbar-thumb {
-      background: #c1c1c1;
-      border-radius: 4px;
-    }
-
-    .customScroll::-webkit-scrollbar-thumb:hover {
-      background: #a8a8a8;
-    }
-  `}</style>
-          <ul className="customScroll">
-            {item.list.map((list, i) => (
-              <li key={i}>
-                <span className="icon">
-                  <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M17 8.26858V9.00458C16.999 10.7297 16.4404 12.4083 15.4075 13.79C14.3745 15.1718 12.9226 16.1826 11.2683 16.6717C9.61394 17.1608 7.8458 17.1021 6.22757 16.5042C4.60934 15.9064 3.22772 14.8015 2.28877 13.3542C1.34981 11.907 0.903833 10.195 1.01734 8.47363C1.13085 6.75223 1.79777 5.11364 2.91862 3.80224C4.03948 2.49083 5.55423 1.57688 7.23695 1.1967C8.91967 0.816507 10.6802 0.990449 12.256 1.69258M17 2.60458L9 10.6126L6.6 8.21258"
-                      stroke="#560CE3"
-                      strokeWidth="1.8"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </span>
-                <span>{list}</span>
-              </li>
-            ))}
-          </ul>
           {/* <div className="tg-pricing-head">
           <h4 className="tg-pricing-title mb-15">{item.suitable}</h4>
         </div> */}
--- src/data/PricingData.ts
+++ src/data/PricingData.ts
@@ -6,6 +6,7 @@ interface DataType {
   price: number;
   list: string[];
   suitable?: string;
+  
 }
 
 const pricing_data: DataType[] = [
