4e0f9915c79c0e4da961b365a099436478b3fcf1
WaleedGhori <waleedghori4@gmail.com>
2025-11-24 11:09:39 +0500

    blog pagination done

--- src/app/blog-details/BlogDetailsWrapper.tsx
+++ src/app/blog-details/BlogDetailsWrapper.tsx
@@ -1,5 +1,6 @@
 "use client";
 import BlogDetails from "@/components/blogs/blog-details";
+import Loading from "@/components/loading/Loading";
 import { useSearchParams } from "next/navigation";
 
 const BlogDetailsWrapper = () => {
@@ -7,7 +8,7 @@ const BlogDetailsWrapper = () => {
   const id = params.get("id");
   const url = params.get("url");
 
-  if (!id || !url) return <div>Loading...</div>;
+  if (!id || !url) return <Loading loadingText={"Loading..."}/>;
 
   return <BlogDetails id={id} url={url} />;
 };
